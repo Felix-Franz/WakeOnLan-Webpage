094b0ac62607e0820f7767a7e92de9fe236c5c79
Felix Franz <felix-franz@t-online.de>
2018-02-07 19:45:55 +0100

    added backend output of available devices

--- wol.php
+++ wol.php
@@ -1,8 +1,27 @@
 <?php
 require_once('config.php');
 
-var_dump(Config::$users);
-echo("<br /> <br />");
-var_dump(Config::$devices);
+//var_dump(Config::$users);
+//echo("<br /> <br />");
+//var_dump(Config::$devices);
 
+
+function handleGet(){
+	return Config::$devices;
+}
+
+function handleRequest(){
+	 switch($_SERVER['REQUEST_METHOD']){
+		case "GET":
+			echo(json_encode(handleGet()));
+			break;
+		default:
+			http_response_code(405);
+			echo("Method not allowed!");
+			break;
+	 }
+	
+}
+
+handleRequest();
 ?>
