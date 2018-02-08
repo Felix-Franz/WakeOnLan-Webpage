66074d54e52702b89281459ce5f778c119183a38
Felix Franz <felix-franz@t-online.de>
2018-02-08 17:51:10 +0000

    added sending wol packet to backend

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 A webpage that should be displayed if a server (behind a reverse proxy) is powered off to start it remotly. It is also possible to start multiple instances of servers.
 
 
+## Instalation
+
+- Install `sudo apt install wakeonlan`
+
 ## Technology
 
 - [Material Design Lite](https://getmdl.io/)
--- api/wol.php
+++ api/wol.php
@@ -6,14 +6,35 @@ require_once('config.php');
 //var_dump(Config::$devices);
 
 
-function handleGet(){
-	return Config::$devices;
+function handlePost($body){
+	$hardwareAddress = Config::$devices[$body->id][hardwareAddress];
+	exec("wakeonlan " . $hardwareAddress, $output, $errorCode);
+	if ($errorCode == 0) return $output[0];
+	else {
+		http_response_code(500);
+		return "could not execute " . "wakeonlan " . $hardwareAddress . "!";
+	}
+}
+
+function handleGet($body){
+	var_dump($body);
+	$devices = Config::$devices;
+	for ($i=0; $i<count($devices); $i++){
+		$devices[$i]["id"] = $i;
+	}
+	header('Content-Type: application/json');
+	return json_encode($devices);
 }
 
 function handleRequest(){
-	 switch($_SERVER['REQUEST_METHOD']){
+	$rawBody = file_get_contents('php://input');
+	$body = json_decode($rawBody);
+	switch($_SERVER['REQUEST_METHOD']){
 		case "GET":
-			echo(json_encode(handleGet()));
+			echo(handleGet($body));
+			break;
+		case "POST":
+			echo(handlePost($body));
 			break;
 		default:
 			http_response_code(405);
